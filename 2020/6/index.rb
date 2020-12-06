698ada6e9c136eb0f305acef7c5684874316be5c
Matthew Farley <matthewthomasfarley@gmail.com>
2020-12-06 00:43:07 -0500

    Add ruby solution for 2020/6

---
+++ 2020/6/index.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+public
+
+def main(puzzle_variant = '1')
+  case puzzle_variant
+  when '1' then variant_one
+  when '2' then variant_two
+  else raise PuzzleVariantError.new 'Invalid puzzle variant provided. Valid values are "1", and "2"'
+  end
+
+rescue StandardError => error
+  error
+end
+
+def variant_one
+  groups.map(&count_any_unique_yes).sum
+end
+
+def variant_two
+  groups.map(&count_all_yes).sum
+end
+
+private
+
+def groups
+  @groups ||= File.open("#{__dir__}/input.txt", &:read).split("\n\n")
+end
+
+def count_any_unique_yes
+  ->(group) { group.tr("\n", '').split('').uniq.count }
+end
+
+def count_all_yes
+  ->(group) { group.split("\n").map { |answer| answer.split('') }.reduce(&:&).count }
+end
+
+class PuzzleVariantError < StandardError; end
