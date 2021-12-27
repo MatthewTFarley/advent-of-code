fb0051d5fb0951ac19fd598b1d094291dec30cfd
Matthew Farley <matthewthomasfarley@gmail.com>
2021-12-26 23:30:51 -0500

    Add stub files for 2021/23

---
+++ 2021/23/index.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+public
+
+def puzzle_one
+  # solved manually
+end
+
+def puzzle_two
+  # solved manually
+end
+
+private
+
+module Input
+  class << self
+  end
+end
