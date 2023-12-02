437ea772ceab968c6f4b978d063ec25c54339ffa
Matthew Farley <matthewthomasfarley@gmail.com>
2023-12-01 23:30:06 -0500

    Refactor TS cli code

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
-const [year, day, part] = process.argv.slice(2);
+import { runPuzzleFunction } from "./utils.ts";
 
-const { partOne, partTwo } = (await import(`./${year}/${day}/index.ts`)) as {
-  partOne: () => Promise<unknown>;
-  partTwo: () => Promise<unknown>;
-};
+const result = await runPuzzleFunction();
 
-if (!["1", "2"].includes(part)) {
-  throw new Error(`invalid part "${part}". Must be "1" or "2"`);
-}
-
-const func = part === "1" ? partOne : partTwo;
-
-console.log(await func());
+console.log(result);
--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 import { readFile } from "node:fs/promises";
 import path from "node:path";
 
-const [year, day, , filename] = process.argv.slice(2);
+const [year, day, part, filename] = process.argv.slice(2);
 
 export async function getInput(): Promise<string> {
   const filepath = path.join(process.cwd(), `${year}/${day}/${filename}`);
@@ -11,4 +11,19 @@ export async function getInput(): Promise<string> {
 export async function getLinesFromInput() {
   const input = await getInput();
   return input.trimEnd().split("\n");
+}
+
+export async function runPuzzleFunction(): Promise<unknown> {
+  const { partOne, partTwo } = (await import(`./${year}/${day}/index.ts`)) as {
+    partOne: () => Promise<unknown>;
+    partTwo: () => Promise<unknown>;
+  };
+
+  if (["1", "2"].includes(part)) {
+    return (part === "1" ? partOne : partTwo)();
+  }
+
+  throw new Error(
+    `Invalid part "${part}" provided. Part must be either "1" or "2".`,
+  );
 }
